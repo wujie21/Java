0c20948c841923896e1ae4e565a1ff719f5e4569
DoDo <DoDo.com>
2022-12-03 23:05:18 +0800

    ok

---
+++ 基础语法/Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+  public static void main(String[] args) {
+    byte v1 = 127;
+    short v2 = 128;
+    int v3 = 129;
+    long v4 = 130L;
+    float v5 = 135.99F;
+    double v6 = 140.99D;
+    char v7 = 'w';
+    boolean v8 = true;
+    System.out.println(v6);
+  }
+}
